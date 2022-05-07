790f16495efd37c9469d4f284950bc27dd0f1b1f
Erik Martinez <61127579+ErikMtz99@users.noreply.github.com>
2022-05-07 15:17:47 -0500

    Agregar cambios de bootstrap

--- js/menu.component.js
+++ js/menu.component.js
@@ -1,21 +1,21 @@
 app.component('menu-component', {
     template: `<!-- A grey horizontal navbar that becomes vertical on small screens -->
-    <nav class="navbar navbar-expand-sm bg-light">
+    <nav class="navbar fixed-top navbar-expand-lg" style="background-color: #e3f2fd;">
         <div class="container-fluid">
           <!-- Links -->
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link" href="index.html">Inicio</a>
+              <a class="nav-link" style ="color:black;" href="index.html">Inicio</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="customer.html">Base de Datos</a>
+              <a class="nav-link" style ="color:black;"href="customer.html">Inventario</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="about.html">Sobre la página</a>
+              <a class="nav-link" style ="color:black;"href="about.html">Sobre la Tienda</a>
             </li>
           </ul>
           <div class="end-text">
-            <b>{{ user }}</b> <button class="btn btn-outline-primary btn-sm" @click="logOut()">Log out</button>
+            <b>{{ user }}</b> <button class="btn btn-outline-primary btn-sm" style ="color:black;" @click="logOut()">Log out</button>
           </div>
         </div> 
     </nav>`,
